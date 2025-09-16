09a63ab1323dc4849b2f26427133d636a5320524
Kawan Mendes dos Santos <kawanmendes748@gmail.com>
2025-09-16 11:29:36 -0300

    atividades

--- projeto/projeto04.py
+++ projeto/projeto04.py
@@ -1,34 +1,52 @@
 class Leitor :
+    
     leitores = []
+
 
     def __init__(self, id, nome , telefone):
         self.id = id
         self.nome = nome
         self.telefone = telefone
+        self.leitores.append(self)
+
        
     def cadastrar_leitor(self ,id , nome , telefone):
         novo_leitor = Leitor(id , nome , telefone)
-        self.leitores.append(novo_leitor)
-        for i in self.leitores:
-            print(i)
-       
-
-    def atualizar_leitor(self, id , nome , telefone):
+        return novo_leitor
+    
+    def atualizar_leitor(self, id, nome, telefone):
         for leitor in self.leitores:
             if leitor.id == id:
-                leitor.cadastrar_leitor()
-               
-     
-    def deletar_leitor():
-        pass
-    def consultar_leitor():
-        pass
+                leitor.nome = nome
+                leitor.telefone = telefone
+                return True
+        return False
+                
+    def listar_leitores(self):
+        for leitor in self.leitores:
+            print(leitor)
 
+
+    def deletar_leitor(self, id ):
+        for leitor in Leitor.leitores:
+            if leitor.id == id :
+                Leitor.leitores.remove(leitor)
+                print(f'o usuario com o id {id} foi removido')
+                return True
+        return False    
+       
+           
+
+    def consultar_leitor(self , id):
+        for leitor in Leitor.leitores:
+            if leitor.id == id :
+                print(f'o leitor foi encontrado {leitor}')
+                return True
+        print(f'o usuario nao foi encontrado')
+        return False
 
     def __str__(self):
         return  f"ID: {self.id}, Nome: {self.nome}, Telefone: {self.telefone}"
-
-
 class Livro:
    def __init__(self,isbn,titulo,autores,edicao,qtd_exemplar):
        self.isbn = isbn
@@ -49,5 +67,7 @@ class Emprestimo :
 
 leitor01 = Leitor(1,'rubinho marthelo', 6666666)
 leitor01.cadastrar_leitor(2 ,'kawan' , 545454)
-leitor01.atualizar_leitor(1, 'marthelo', 6582984)
-print(leitor01)
+leitor01.consultar_leitor(2)
+
+
+    
