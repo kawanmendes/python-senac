3c15f2be5b3b8d7648bf0231a55299f2b8c8c318
Kawan Mendes <kawanmendes748@gmail.com>
2025-09-16 23:03:12 -0300

    atividades

--- projeto/projeto04.py
+++ projeto/projeto04.py
@@ -48,15 +48,56 @@ class Leitor :
     def __str__(self):
         return  f"ID: {self.id}, Nome: {self.nome}, Telefone: {self.telefone}"
 class Livro:
-   def __init__(self,isbn,titulo,autores,edicao,qtd_exemplar):
-       self.isbn = isbn
-       self.titulo = titulo
-       self.autores = autores
-       self.edicao = edicao
-       self.qtd_exemplar = qtd_exemplar
-       pass
-   
+    livros = []
+    def __init__(self,isbn,titulo,autores,edicao,qtd_exemplar):
+        self.isbn = isbn
+        self.titulo = titulo
+        self.autores = autores
+        self.edicao = edicao
+        self.qtd_exemplar = qtd_exemplar
+        self.livros.append(self)
 
+    def cadastrar_livro(self,isbn,titulo,autores,edicao,qtd_exemplar):
+        novo_livro = Livro(isbn,titulo,autores,edicao,qtd_exemplar)
+        return novo_livro
+    
+    def atualizar_livro(self,isbn,titulo,autores,edicao,qtd_exemplar):
+        for livro in self.livros:
+            if livro.isbn == isbn:
+                livro.titulo = titulo
+                livro.autores = autores
+                livro.edicao = edicao
+                livro.qtd_exemplar = qtd_exemplar
+                return True
+        return False
+    
+    def listar_livros(self):
+        for livro in self.livros:
+            print(livro)
+    def deletar_livro(self,isbn):
+        for livro in Livro.livros:
+            if livro.isbn == isbn :
+                Livro.livros.remove(livro)
+                print(f'o livro com o livro {livro.titulo} foi removido')
+                return True
+        return False
+    def consultar_livro(self,isbn):
+        for livro in Livro.livros:
+            if livro.isbn == isbn :
+                print(f'o livro foi encontrado {livro}')
+                return True
+        print(f'o livro nao foi encontrado')
+        return False
+    def __str__(self):
+        return f"ISBN: {self.isbn}, Título: {self.titulo}, Autores: {self.autores}, Edição: {self.edicao}, Quantidade de Exemplares: {self.qtd_exemplar}"
+    
+    def verificar_disponibilidade(self , qtd_exemplar):
+        if self.qtd_exemplar > 0 :
+            print(f'o livro {self.titulo} esta disponivel')
+            return True
+        else:
+            print(f'o livro {self.titulo} nao esta disponivel')
+            return False
 class Emprestimo :
      def __init__(self, data_emprestimo, data_devolucao, livro : Livro ,leitor : Leitor):
        self.data_emprestimo = data_emprestimo
@@ -64,10 +105,6 @@ class Emprestimo :
        self.livro = livro
        self.leitor = leitor
 
-
-leitor01 = Leitor(1,'rubinho marthelo', 6666666)
-leitor01.cadastrar_leitor(2 ,'kawan' , 545454)
-leitor01.consultar_leitor(2)
 
 
     
